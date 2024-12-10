aa917e3c1fef2d7dd0442c74f16e4ed7657e1954
Michael Flisar <Michael.Flisar@knapp.com>
2024-12-10 10:40:24 +0100

    bug reproduction

--- demo-modalsheet/src/commonMain/kotlin/ModalBottomSheetDemo.kt
+++ demo-modalsheet/src/commonMain/kotlin/ModalBottomSheetDemo.kt
@@ -8,6 +8,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,7 +28,7 @@ import com.composables.core.SheetDetent.Companion.FullyExpanded
 import com.composables.core.SheetDetent.Companion.Hidden
 
 private val Peek = SheetDetent("peek") { containerHeight, sheetHeight ->
-    containerHeight * 0.6f
+    containerHeight * 0.5f
 }
 
 @Composable
@@ -33,9 +38,29 @@ fun ModalBottomSheetDemo() {
             .fillMaxSize()
             .background(Brush.linearGradient(listOf(Color(0xFF800080), Color(0xFFDA70D6))))
     ) {
+
+        val expandInitially = true
+        val dismissAllowed = remember { mutableStateOf(true) }
+
+
+        val shouldDismissOnBackPress by remember {
+            derivedStateOf { dismissAllowed.value }
+        }
+        val shouldDismissOnClickOutside by remember {
+            derivedStateOf {  dismissAllowed.value }
+        }
+
+
         val modalSheetState = rememberModalBottomSheetState(
-            initialDetent = Hidden,
-            detents = listOf(Hidden, Peek, FullyExpanded)
+            initialDetent = if (expandInitially || Peek == null) SheetDetent.FullyExpanded else Peek,
+            detents = listOf(Hidden, Peek, FullyExpanded),
+            confirmDetentChange = {
+                val confirm = if (it == SheetDetent.Hidden) {
+                    true
+                } else true
+                println("TEST - confirmDetentChange = $confirm | ${it.identifier}")
+                confirm
+            }
         )
 
         Box(
@@ -52,7 +77,39 @@ fun ModalBottomSheetDemo() {
 
         val isCompact = maxWidth < 600.dp
 
-        ModalBottomSheet(state = modalSheetState) {
+        LaunchedEffect(modalSheetState.isIdle) {
+
+            println("TEST - isIdle = ${modalSheetState.isIdle} | target = ${modalSheetState.currentDetent.identifier}")
+            if (modalSheetState.isIdle &&
+                modalSheetState.currentDetent == SheetDetent.Hidden
+            ) {
+                println("TEST - show again")
+                modalSheetState.currentDetent = SheetDetent.FullyExpanded
+                //bottomSheetState.animateTo(SheetDetent.FullyExpanded)
+            }
+        }
+
+        LaunchedEffect(modalSheetState.offset) {
+            println("TEST - bottomSheetState.offset = ${modalSheetState.offset}")
+        }
+        LaunchedEffect(modalSheetState.currentDetent) {
+            println("TEST - bottomSheetState.currentDetent = ${modalSheetState.currentDetent.identifier}")
+
+        }
+        LaunchedEffect(modalSheetState.progress) {
+            println("TEST - bottomSheetState.progress = ${modalSheetState.progress}")
+        }
+
+        ModalBottomSheet(
+            state = modalSheetState,
+            properties = ModalSheetProperties(
+                dismissOnBackPress = shouldDismissOnBackPress,
+                dismissOnClickOutside = shouldDismissOnClickOutside
+            ),
+            onDismiss = {
+                println("TEST - onDismiss callback...")
+            }
+        ) {
             Scrim(scrimColor = Color.Black.copy(0.3f), enter = fadeIn(), exit = fadeOut())
 
             Sheet(
@@ -69,13 +126,16 @@ fun ModalBottomSheetDemo() {
                     .fillMaxWidth()
                     .imePadding(),
             ) {
-                Box(Modifier.fillMaxWidth().height(600.dp), contentAlignment = Alignment.TopCenter) {
-                    DragIndication(
-                        modifier = Modifier.padding(top = 22.dp)
-                            .background(Color.Black.copy(0.4f), RoundedCornerShape(100))
-                            .width(32.dp)
-                            .height(4.dp)
-                    )
+                Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.TopCenter) {
+                    Column {
+                        DragIndication(
+                            modifier = Modifier.padding(top = 22.dp)
+                                .background(Color.Black.copy(0.4f), RoundedCornerShape(100))
+                                .width(32.dp)
+                                .height(4.dp)
+                        )
+                        BasicText("Content")
+                    }
                 }
             }
         }
